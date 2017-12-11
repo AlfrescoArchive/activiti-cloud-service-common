a4c478cc2bd489d2ced2441ce6f3e38508e7a4aa
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2017-12-11 12:16:41 +0000

    Add extra check on authentication test

--- activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakAuthenticationProviderTest.java
+++ activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakAuthenticationProviderTest.java
@@ -1,5 +1,8 @@
 package org.activiti.cloud.services.identity.keycloak;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.keycloak.KeycloakPrincipal;
@@ -8,17 +11,17 @@ import org.keycloak.adapters.spi.KeycloakAccount;
 import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.AccessToken;
-
-import java.util.Arrays;
-import java.util.HashSet;
+import org.springframework.security.core.Authentication;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KeycloakAuthenticationProviderTest {
 
-    private KeycloakActivitiAuthenticationProvider keycloakActivitiAuthenticationProvider = new KeycloakActivitiAuthenticationProvider();
+    private KeycloakActivitiAuthenticationProvider keycloakActivitiAuthenticationProvider = spy(new KeycloakActivitiAuthenticationProvider());
     private KeycloakAuthenticationToken token;
     private RefreshableKeycloakSecurityContext keycloakSecurityContext;
 
@@ -32,26 +35,31 @@ public class KeycloakAuthenticationProviderTest {
     }
 
     @Test
-    public void testAuthenticate(){
+    public void authenticateShouldUseNameFromAuthenticationWhenPreferredUserNameIsNotSet(){
 
-        assertThat(keycloakActivitiAuthenticationProvider.authenticate(token)).isNotNull();
+        //when
+        Authentication authentication = keycloakActivitiAuthenticationProvider.authenticate(token);
 
-        AccessToken accessToken = mock(AccessToken.class);
-        when(keycloakSecurityContext.getToken()).thenReturn(accessToken);
-        when(accessToken.getPreferredUsername()).thenReturn("bob");
-
-        assertThat(keycloakActivitiAuthenticationProvider.authenticate(token)).isNotNull();
+        //then
+        assertThat(authentication).isNotNull();
+        verify(keycloakActivitiAuthenticationProvider).setAuthenticatedUserId("bob");
 
     }
 
     @Test
-    public void testAuthenticateWithPreferredUsername(){
+    public void authenticateShouldUsePreferredUsernameWhenSet(){
 
+        //given
         AccessToken accessToken = mock(AccessToken.class);
         when(keycloakSecurityContext.getToken()).thenReturn(accessToken);
-        when(accessToken.getPreferredUsername()).thenReturn("bob");
+        when(accessToken.getPreferredUsername()).thenReturn("bob@any.org");
 
-        assertThat(keycloakActivitiAuthenticationProvider.authenticate(token)).isNotNull();
+        //when
+        Authentication authentication = keycloakActivitiAuthenticationProvider.authenticate(token);
+
+        //then
+        assertThat(authentication).isNotNull();
+        verify(keycloakActivitiAuthenticationProvider).setAuthenticatedUserId("bob@any.org");
 
     }
 }
