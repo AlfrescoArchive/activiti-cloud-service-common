118fcf1623eae8684d96107196c9895a2b560703
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-27 13:50:15 +0100

    test that wildcard can be used as policy (applied at next service layer)

--- activiti-cloud-services-security-policies/src/test/java/org/activiti/cloud/services/security/SecurityPoliciesServiceIT.java
+++ activiti-cloud-services-security-policies/src/test/java/org/activiti/cloud/services/security/SecurityPoliciesServiceIT.java
@@ -131,4 +131,23 @@ public class SecurityPoliciesServiceIT {
         assertThat(keys.get(rb1)).contains("SimpleProcessYML1");
         assertThat(keys.get(rb1)).contains("SimpleProcessYML2");
     }
+
+    @Test
+    public void shouldGetWildcardByUserAndPolicies() throws Exception {
+
+        Map<String,Set<String>> keys = securityPoliciesService.getProcessDefinitionKeys("gary",null, Arrays.asList(SecurityPolicy.WRITE,SecurityPolicy.READ));
+
+        assertThat(keys.get(rb1)).hasSize(1);
+        assertThat(keys.get(rb1)).contains("*");
+    }
+
+    @Test
+    public void shouldGetWildcardByGroupsAndMinPolicy() throws Exception {
+
+        Map<String,Set<String>> keys = securityPoliciesService.getProcessDefinitionKeys(null,Arrays.asList("accounts","nonexistent"), SecurityPolicy.READ);
+
+        assertThat(keys.get(rb1)).hasSize(1);
+        assertThat(keys.get(rb1)).contains("*");
+    }
+
 }
