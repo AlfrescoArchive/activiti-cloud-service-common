65e8cdabe9fb3ec4e2e12a763c3af93dcf98b40a
Igor Dianov <igor@dianov.com>
2019-05-27 19:04:51 -0700

    feat: add KeycloakSecurityManagerImpl.getAuthenticatedUserGroups method

--- activiti-cloud-services-common-security-keycloak/src/main/java/org/activiti/cloud/services/common/security/keycloak/KeycloakSecurityManagerImpl.java
+++ activiti-cloud-services-common-security-keycloak/src/main/java/org/activiti/cloud/services/common/security/keycloak/KeycloakSecurityManagerImpl.java
@@ -1,7 +1,18 @@
 package org.activiti.cloud.services.common.security.keycloak;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.activiti.api.runtime.shared.security.SecurityManager;
 import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class KeycloakSecurityManagerImpl implements SecurityManager {
@@ -18,4 +29,20 @@ public class KeycloakSecurityManagerImpl implements SecurityManager {
         }
         return "";
     }
+    
+    @Override
+    public List<String> getAuthenticatedUserGroups() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                       .map(SecurityContext::getAuthentication)
+                       .map(Authentication::getCredentials)
+                       .filter(KeycloakSecurityContext.class::isInstance)
+                       .map(KeycloakSecurityContext.class::cast)
+                       .map(KeycloakSecurityContext::getToken)
+                       .map(AccessToken::getOtherClaims)
+                       .map(it -> it.get("groups"))
+                       .filter(Collection.class::isInstance)
+                       .map(Collection.class::cast)
+                       .map(it -> Collections.unmodifiableList(new ArrayList<String>(it)))
+                       .orElse(Collections.emptyList());
+    }    
 }
