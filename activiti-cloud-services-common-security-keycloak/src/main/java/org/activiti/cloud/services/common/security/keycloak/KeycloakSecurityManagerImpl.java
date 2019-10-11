fc07fdf513fa3ae4791c79ae4c74c44d815662fd
Igor Dianov <igor@dianov.com>
2019-10-11 12:12:47 -0700

    refactor: implemented KeycloakSecurityManagerImpl provider

--- activiti-cloud-services-common-security-keycloak/src/main/java/org/activiti/cloud/services/common/security/keycloak/KeycloakSecurityManagerImpl.java
+++ activiti-cloud-services-common-security-keycloak/src/main/java/org/activiti/cloud/services/common/security/keycloak/KeycloakSecurityManagerImpl.java
@@ -1,19 +1,18 @@
 package org.activiti.cloud.services.common.security.keycloak;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
 import org.activiti.api.runtime.shared.security.SecurityManager;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.representations.AccessToken;
-import org.springframework.context.annotation.Primary;
+import org.keycloak.representations.AccessToken.Access;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 public class KeycloakSecurityManagerImpl implements SecurityManager {
 
@@ -22,7 +21,7 @@ public class KeycloakSecurityManagerImpl implements SecurityManager {
         if (SecurityContextHolder.getContext().getAuthentication() != null) {
             Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             if (principal instanceof KeycloakPrincipal) {
-                KeycloakPrincipal keycloakPrincipal = (KeycloakPrincipal) principal;
+                KeycloakPrincipal<?> keycloakPrincipal = (KeycloakPrincipal<?>) principal;
                 return keycloakPrincipal.getKeycloakSecurityContext().getToken().getPreferredUsername();
             }
             return SecurityContextHolder.getContext().getAuthentication().getName();
@@ -31,17 +30,31 @@ public class KeycloakSecurityManagerImpl implements SecurityManager {
     }
     
     @Override
-    public Optional<List<String>> getAuthenticatedUserGroups() {
+    public Collection<String> getAuthenticatedUserGroups() {
+        return getAuthenticatedUserAccessToken().map(AccessToken::getOtherClaims)
+                                                .map(it -> it.get("groups"))
+                                                .filter(Collection.class::isInstance)
+                                                .map(Collection.class::cast)
+                                                .map(it -> Collections.unmodifiableCollection(new ArrayList<String>(it)))
+                                                .orElseThrow(() -> new SecurityException("Invalid groups claims in access token"));
+    }
+
+    @Override
+    public Collection<String> getAuthenticatedUserRoles() {
+        return getAuthenticatedUserAccessToken().map(AccessToken::getRealmAccess)
+                                                .map(Access::getRoles)
+                                                .map(it -> Collections.unmodifiableCollection(new ArrayList<String>(it)))
+                                                .orElseThrow(() -> new SecurityException("Invalid realm access roles in access token"));
+    }
+
+    private Optional<AccessToken> getAuthenticatedUserAccessToken() {
         return Optional.ofNullable(SecurityContextHolder.getContext())
                        .map(SecurityContext::getAuthentication)
                        .map(Authentication::getCredentials)
                        .filter(KeycloakSecurityContext.class::isInstance)
                        .map(KeycloakSecurityContext.class::cast)
                        .map(KeycloakSecurityContext::getToken)
-                       .map(AccessToken::getOtherClaims)
-                       .map(it -> it.get("groups"))
-                       .filter(Collection.class::isInstance)
-                       .map(Collection.class::cast)
-                       .map(it -> Collections.unmodifiableList(new ArrayList<String>(it)));
+                       .filter(AccessToken::isActive);
     }
+    
 }
