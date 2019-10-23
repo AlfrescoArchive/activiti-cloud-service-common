8676dccb4a0506cf34ea6e0e632bd16c4213fbde
Igor Dianov <igor@dianov.com>
2019-10-22 20:41:40 -0700

    feat(test): Added test coverage for KeycloakSecurityManagerImplTest

--- activiti-cloud-services-common-security-keycloak/src/test/java/org/activiti/cloud/services/common/security/keycloak/test/KeycloakSecurityManagerImplTest.java
+++ activiti-cloud-services-common-security-keycloak/src/test/java/org/activiti/cloud/services/common/security/keycloak/test/KeycloakSecurityManagerImplTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.activiti.cloud.services.common.security.keycloak.test;
 
@@ -6,6 +21,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 
 import org.activiti.api.runtime.shared.security.SecurityManager;
 import org.activiti.cloud.services.common.security.keycloak.KeycloakSecurityManagerImpl;
+import org.activiti.cloud.services.common.security.keycloak.test.support.WithMockKeycloakUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +30,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -31,6 +49,44 @@ public class KeycloakSecurityManagerImplTest {
     @Test
     public void contextLoads() {
         assertThat(securityManager).isInstanceOf(KeycloakSecurityManagerImpl.class);
+    }
+
+    @Test
+    @WithMockKeycloakUser(username = "hruser")
+    public void testGetAuthenticatedUserId() {
+        // given
+        
+        // when
+        String result = securityManager.getAuthenticatedUserId();
+        
+        // then
+        assertThat(result).isEqualTo("hruser");
+    }
+    
+    @Test
+    @WithMockKeycloakUser(groups = {"hr", "admins"})
+    public void testGetAuthenticatedUserGroups() {
+        // given
+        
+        // when
+        List<String> result = securityManager.getAuthenticatedUserGroups();
+        
+        // then
+        assertThat(result).isNotEmpty()
+                          .containsExactly("hr", "admins");
+    }
+    
+    @Test
+    @WithMockKeycloakUser(roles = {"ACTIVITI_USER"})
+    public void testGetAuthenticatedUserRoles() {
+        // given
+        
+        // when
+        List<String> result = securityManager.getAuthenticatedUserRoles();
+        
+        // then
+        assertThat(result).isNotEmpty()
+                          .containsExactly("ACTIVITI_USER");
     }
     
     @Test
